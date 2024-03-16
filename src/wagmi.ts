d391494562146fc04befb0f315a993946a69b45f
Paul Davis <pd@perfectdimension.com>
2024-03-16 23:41:52 +0000

    Remove scaffold wagmi config.

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,22 +2,22 @@ import { http, createConfig } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  connectors: [
-    injected(),
-    coinbaseWallet({ appName: "ZenMon" }),
-    walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
-  ],
-  ssr: true,
-  transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
-  },
-});
+// export const config = createConfig({
+//   chains: [mainnet, sepolia],
+//   connectors: [
+//     injected(),
+//     coinbaseWallet({ appName: "ZenMon" }),
+//     walletConnect({ projectId: "7b89f04f8986abd750f74d7668f82275" }),
+//   ],
+//   ssr: true,
+//   transports: {
+//     [mainnet.id]: http(),
+//     [sepolia.id]: http(),
+//   },
+// });
 
-declare module "wagmi" {
-  interface Register {
-    config: typeof config;
-  }
-}
+// declare module "wagmi" {
+//   interface Register {
+//     config: typeof config;
+//   }
+// }
